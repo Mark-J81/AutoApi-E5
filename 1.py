7a2cc1c4fea811136eb98a4a2764cec7231786a0
Jayson <18487449145@163.com>
2021-10-21 11:07:42 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -79,3 +79,4 @@ for _ in range(3):
     main()
 
 requests.get('https://sc.ftqq.com/SCT65547TPBQlXuJoG2Mk7AZ0Y1eKmWl3.send?text=E5续期调用API权限10次运行完毕（163）')
+requests.get('http://www.pushplus.plus/send?token=0dacd84ff358406fa3b69f8fab31776f&title=163&content=E5续期调用API权限10次运行完毕')
